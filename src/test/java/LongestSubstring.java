ecd37a9284386bdb8bf2d109cded1676c8ba4dd2
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-05-23 17:32:16 -0400

    added an array and windowed iplementation

--- src/test/java/LongestSubstring.java
+++ src/test/java/LongestSubstring.java
@@ -1,17 +1,51 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class LongestSubstring {
     public static int lengthOfLongestSubstring(String s) {
 
-        if(s.isEmpty()) return 0;
-        if(s.length()==1) return 1;
+        if (s.isEmpty()) return 0;
+        if (s.length() == 1) return 1;
 
-        String longest = s.substring(0,1);
+        String longest = s.substring(0, 1);
+        HashSet<Character> current = new HashSet<>();
+        var list = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+
+        for (var outerIterator = list.listIterator(); outerIterator.hasNext(); outerIterator.next()) {
+            for (var innerIterator = list.listIterator(outerIterator.nextIndex()); innerIterator.hasNext(); ) {
+                char c = innerIterator.next();
+                if (current.contains(c)) {
+                    current.clear();
+                    break;
+                }
+                current.add(c);
+
+                if (longest.length() < current.size()) {
+                    longest = current.stream().collect(Collector.of(
+                            StringBuilder::new,
+                            StringBuilder::append,
+                            StringBuilder::append,
+                            StringBuilder::toString
+                            )
+                    );
+                    if (s.length() - outerIterator.nextIndex() < longest.length()) break;
+                }
+            }
+        }
+        return longest.length();
+    }
+
+    public static int lengthOfLongestSubstring_arrayImplementation(String s) {
+        if (s.isEmpty()) return 0;
+        if (s.length() == 1) return 1;
+
+        String longest = s.substring(0, 1);
         HashSet<Character> current = new HashSet<>();
 
-        for(int outerIterator = 0; outerIterator < s.length(); outerIterator++){
-            for(int innerIterator = outerIterator ; innerIterator < s.length(); innerIterator++) {
+        for (int outerIterator = 0; outerIterator < s.length(); outerIterator++) {
+            for (int innerIterator = outerIterator; innerIterator < s.length(); innerIterator++) {
                 char c = s.charAt(innerIterator);
                 if (current.contains(c)) {
                     current.clear();
@@ -27,10 +61,62 @@ public class LongestSubstring {
                             StringBuilder::toString
                             )
                     );
-                    if(s.length()-outerIterator < longest.length()) break;
+                    if (s.length() - outerIterator < longest.length()) break;
                 }
             }
         }
         return longest.length();
+    }
+
+    public static int lengthOfLongestSubstring_kRepetitionsAllowed(String s, int k) {
+        if (s.isEmpty()) return 0;
+        if (s.length() == 1) return 1;
+
+        int longest = 1;
+        HashMap<Character, Integer> current = new HashMap();
+
+        for (int outerIterator = 0; outerIterator < s.length(); outerIterator++) {
+            for (int innerIterator = outerIterator; innerIterator < s.length(); innerIterator++) {
+                Character c = s.charAt(innerIterator);
+                if (current.containsKey(c) && current.get(c) == k) {
+                    current.clear();
+                    break;
+                } else if (current.containsKey(c)) {
+                    current.replace(c, current.get(c) + 1);
+                } else {
+                    current.put(c, 1);
+                }
+
+                int currentLength = current.values().stream().reduce(Integer::sum).get();
+                if (longest < currentLength) {
+                    longest = currentLength;
+                }
+            }
+        }
+        return longest;
+    }
+
+    public static int lengthOfLongestSubstring_windowed(String s){
+        if (s.isEmpty()) return 0;
+        if (s.length() == 1) return 1;
+
+        int longest = 1;
+        var visited = new HashSet<Character>();
+
+        for (int leftIterator = 0; leftIterator < s.length();) {
+            for (int rightIterator = leftIterator; rightIterator < s.length(); rightIterator++) {
+                char left = s.charAt(leftIterator);
+                char right = s.charAt(rightIterator);
+                if (visited.contains(right)) {
+                    visited.remove(left);
+                    leftIterator++;
+                }
+                visited.add(right);
+                if (longest < visited.size()) {
+                    longest = visited.size();
+                }
+            }
+        }
+        return longest;
     }
 }
--- src/test/java/LongestSubstringTest.java
+++ src/test/java/LongestSubstringTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class LongestSubstringTest {
 
@@ -13,5 +18,49 @@ class LongestSubstringTest {
         Assertions.assertEquals(0, LongestSubstring.lengthOfLongestSubstring(""));
         Assertions.assertEquals(2, LongestSubstring.lengthOfLongestSubstring("ab"));
         Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring("dvdf"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_arrayImplementation() {
+        Assertions.assertEquals(6, LongestSubstring.lengthOfLongestSubstring_arrayImplementation("asjrgapa"));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_arrayImplementation("abcabcbb"));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_arrayImplementation("pwwkew"));
+        Assertions.assertEquals(0, LongestSubstring.lengthOfLongestSubstring_arrayImplementation(""));
+        Assertions.assertEquals(2, LongestSubstring.lengthOfLongestSubstring_arrayImplementation("ab"));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_arrayImplementation("dvdf"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_kRepetitionsAllowed() {
+        Assertions.assertEquals(5, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("eceba", 2));
+        Assertions.assertEquals(6, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("abcabcbb", 2));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("eeee", 3));
+
+
+        //orignal implementation tests
+        Assertions.assertEquals(6, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("asjrgapa", 1));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("abcabcbb", 1));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("pwwkew", 1));
+        Assertions.assertEquals(0, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("", 1));
+        Assertions.assertEquals(2, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("ab", 1));
+        Assertions.assertEquals(3, LongestSubstring.lengthOfLongestSubstring_kRepetitionsAllowed("dvdf", 1));
+    }
+
+    @ParameterizedTest(name = "test {index} the longest non-repeating substring of string {0} has {1} characters")
+    @MethodSource("testCases")
+    void lengthOfLongestSubstring_windowed(int expected, String s) {
+        Assertions.assertEquals(expected, LongestSubstring.lengthOfLongestSubstring_windowed(s));
+    }
+
+    static Stream<Arguments> testCases() {
+        return Stream.of(
+                arguments(4,"jbpnbwwd"),
+                arguments(6, "asjrgapa"),
+                arguments(3, "abcabcbb"),
+                arguments(3, "pwwkew"),
+                arguments(0, ""),
+                arguments(2, "ab"),
+                arguments(3, "dvdf")
+        );
     }
 }
