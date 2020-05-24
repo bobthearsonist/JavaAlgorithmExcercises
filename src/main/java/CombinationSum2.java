428f8f604de7a4324ce543fbe5f259fe2524e54f
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-05-24 16:44:10 -0400

    working except for fucking zeros

--- src/main/java/CombinationSum2.java
+++ src/main/java/CombinationSum2.java
@@ -1,4 +1,9 @@
-import java.util.List;
+import lombok.val;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /*
 Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
 
@@ -8,8 +13,60 @@ Note:
     All numbers (including target) will be positive integers.
     The solution set must not contain duplicate combinations.
  */
+
 public class CombinationSum2 {
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return null;
+        var sorted = new TreeMap<Integer, HashSet<Integer>>();
+
+        IntStream.rangeClosed(0,candidates.length-1).forEach(
+                index -> {
+                    //TODO two n*log(n) traversals for get and put
+                    //TODO check equals behavior
+                    val candidate = candidates[index];
+                    if(sorted.containsKey(candidate)) {
+                        var current = sorted.get(candidate);
+                        current.add(index);
+                        sorted.put(candidate, current);
+                    } else {
+                        var current = new HashSet<Integer>();
+                        current.add(index);
+                        sorted.put(candidate,current);
+                    }
+                }
+        );
+
+        var traversal = sorted.entrySet();
+        var solutions = new HashSet<HashMap<Integer,Integer>>();
+        //note: we are traversing again. we can probably collapse the two loops
+        IntStream.rangeClosed(0,candidates.length-1).forEach( index -> {
+            int candidate = candidates[index];
+            var currentSolution = new HashMap<Integer,Integer>() {{ put(candidate,index); }};
+            var currentSum = candidate;
+
+            for(var current: traversal){
+                var difference = target - currentSum;
+
+                if(difference < 0) {
+                // there are no more valid sums for the current index
+                    break;
+                } else {
+                // add current to the solution
+                    var occurences = current.getValue();
+                    int nextIndex = index;
+                    for(int occurence : occurences) { if(occurence>index) nextIndex=occurence; break; }
+                    if(nextIndex!=index) {
+                        currentSolution.put(current.getKey(), nextIndex);
+                        currentSum += current.getKey();
+                    }
+                }
+
+                if(currentSum == target) {
+                // check the current solution for validity
+                    solutions.add(currentSolution);
+                    break;
+                }
+            }});
+
+        return solutions.stream().map(set->set.keySet().stream().collect(Collectors.toList())).collect(Collectors.toList());
     }
 }
--- src/test/java/CombinationSum2Test.java
+++ src/test/java/CombinationSum2Test.java
@@ -1,4 +1,5 @@
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsIn;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.everyItem;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 /*
 Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
@@ -27,32 +29,38 @@ class CombinationSum2Test {
                             //multiple matches
                             //zero case
                             //values too large
-                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(5,0)),
+                            Arrays.asList(Arrays.asList(0,2,3), Arrays.asList(2,3), Arrays.asList(0,5)),
                             new int[]{2,3,5,0,6,20},
                             5
                     ),
                     arguments(
                             //multiple matches
+                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(4,1)),
+                            new int[]{2,3,4,1},
+                            5
+                    ),
+                    arguments(
+                            //multiple matches
                             //duplicate solutions
-                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(5,0)),
+                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(0,2,3)),
                             new int[]{2,3,0,2,3},
                             5
                     ),
                     arguments(
                             //empty
-                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(5,0)),
+                            Arrays.asList(),
                             new int[]{},
                             0
                     ),
                     arguments(
                             //no solution
-                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(5,0)),
+                            Arrays.asList(),
                             new int[]{1,1,1,1},
                             0
                     ),
                     arguments(
                             //single case is solution
-                            Arrays.asList(Arrays.asList(2,3), Arrays.asList(5,0)),
+                            Arrays.asList(Arrays.asList(1,1)),
                             new int[]{1,1},
                             2
                     )
@@ -62,6 +70,7 @@ class CombinationSum2Test {
     @ParameterizedTest()
     @MethodSource("testCases")
     void combinationSum2(List<List<Integer>> expected, int[] candidates, int target) {
-        Assertions.assertIterableEquals(expected,CombinationSum2.combinationSum2(candidates,target));
+        var actual = CombinationSum2.combinationSum2(candidates,target);
+        MatcherAssert.assertThat(expected,everyItem(IsIn.in(actual)));
     }
 }
