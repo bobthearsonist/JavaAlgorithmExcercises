f4b7b0d4f4dcf77ca217a75ff8c5d64d3316f802
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-07-16 11:17:13 -0400

    another easy level problem

--- src/main/java/SmallestInteger.java
+++ src/main/java/SmallestInteger.java
@@ -1,9 +1,4 @@
 import java.util.Arrays;
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 
 class Solution {
     public int solution(int[] A) {
